e10e6727fc42dcb9c3ed77394a22f330d48822cc
dksung <dksung@jiran.com>
2020-12-01 23:10:12 +0900

    1. Log 업데이트 2. ElctricBill 계산

--- app/src/main/java/com/study/rxandroid/RxJavaUtils/Functions/Zip.java
+++ app/src/main/java/com/study/rxandroid/RxJavaUtils/Functions/Zip.java
@@ -3,9 +3,13 @@ package com.study.rxandroid.RxJavaUtils.Functions;
 import com.study.rxandroid.RxJavaUtils.CommonUtils;
 import com.study.rxandroid.RxJavaUtils.Log;
 
+import java.text.DecimalFormat;
 import java.util.concurrent.TimeUnit;
 
 import io.reactivex.rxjava3.core.Observable;
+
+import static java.lang.Math.max;
+import static java.lang.Math.min;
 
 public class Zip {
 
@@ -43,7 +47,10 @@ public class Zip {
         CommonUtils.sleep(1000);
     }
 
-
+    public static void caleleBill(){
+        ElctricBill eb = new ElctricBill();
+        eb.cal();
+    }
 }
 
 class Shape {
@@ -76,5 +83,51 @@ class Shape {
         if (PENTAGON.equals(shape)) return "-P";
         if (STAR.equals(shape)) return "-S";
         return "";    // 원의 경우
+    }
+}
+
+class ElctricBill{
+
+    private int index = 0;
+
+    public void cal(){
+
+        String[] data = {"100", "300"};
+
+        Observable<Integer> basePrice = Observable.fromArray(data)
+                .map(Integer::parseInt)
+                .map(val -> {
+                            if (val <= 200) return 910;
+                            if (val <= 400) return 1600;
+                            return 7300;
+                        }
+                );
+
+        Observable<Integer> usagePrice = Observable.fromArray(data)
+                .map(Integer::parseInt)
+                .map(val -> {
+                            double series1 = min(200, val) * 93.3;
+                            double series2 = min(200, max(val-200, 0)) * 187.9;
+                            double series3 = min(0, max(val-400, 0)) * 280.56;
+                            return (int)(series1 + series2 + series3);
+                        }
+                );
+
+        Observable<Integer> source = Observable.zip(
+                basePrice,
+                usagePrice,
+                (v1, v2) -> v1 + v2);
+
+        // result:
+        source.map(val -> new DecimalFormat("#,###").format(val))
+                .subscribe(val -> {
+                            StringBuilder sb = new StringBuilder();
+                            sb.append("Usage: " + data[index] + " kWh => ");
+                            sb.append("Price " + val + "원");
+                            Log.i(sb.toString());
+
+                            index++; // 부수 효
+                        }
+                );
     }
 }
